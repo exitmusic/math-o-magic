e8669c3ef3539f81bbbe7209e632af6834a24ec9
Kai Chang <kai.h.chang@gmail.com>
2012-07-29 14:41:50 -0500

    Added timer related functions. TODO: time stuff should be its own model/object.

--- app/models/question-generator.js
+++ app/models/question-generator.js
@@ -3,9 +3,10 @@
  * @constructor
  */
 function QuestionGenerator() {
-  this.timeRemaining;
+  this.timeRemaining; //TODO: time related stuff should be its own object?
 	this.question;
 	this.answer;
+	this.timer; //TODO: time related stuff should be its own object?
 }
 
 /**
@@ -31,14 +32,29 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;  
 }
 
-function initTimer() {
-	
-	
+QuestionGenerator.prototype.init = function() {
+	this.timeRemaining = 10;
+	this.question = generateQuestion();
 }
 
-QuestionGenerator.prototype.init = function() {
-	this.timeRemaining = initTimer();
-	this.question = generateQuestion();
+QuestionGenerator.prototype.startTimer = function() {
+	var timer
+		, thisGenerator = this;
+	
+	clearInterval(thisGenerator.timer); // clear any previous running timers
+	thisGenerator.timeRemaining = 10;
+  
+	timer = setInterval(function() {
+		thisGenerator.timeRemaining -= 1;
+  	if (thisGenerator.timeRemaining === 0) {
+  		clearInterval(thisGenerator.timer);
+  	}
+  }, 1000);
+  this.timer = timer;
+}
+
+QuestionGenerator.prototype.stopTimer = function() {
+	clearInterval(this.timer);
 }
 
 module.exports = QuestionGenerator;
