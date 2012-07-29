e73f399380ad97b15c4236d3f51afa2a66e50ce7
Kai Chang <kai.h.chang@gmail.com>
2012-07-29 12:18:29 -0500

    Better way to get the number of players in the trivia room.

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -1,8 +1,8 @@
 function routes(app, io) {
 	app.get('/', function(req, res){
 		//io.sockets.in(req.sessionID).send('Man, good to see you back!');
-		io.sockets.in('trivia-room').send(Object.keys(io.sockets.in('trivia-room').manager));
-		var numOfPlayers = Object.keys(io.sockets.in('trivia-room').manager.connected).length;
+		io.sockets.in('trivia-room').send(Object.keys(io.sockets.clients('trivia-room')));
+		var numOfPlayers = io.sockets.clients('trivia-room').length;
 		res.render('index', { 
 				title: 'Express'
 			, numOfPlayers: numOfPlayers //TODO: instead of passing as a variable, this should update the view in real time
@@ -10,6 +10,10 @@ function routes(app, io) {
 	});
 	
 	app.post('/start', function(req, res) {
+		
+	});
+	
+	app.post('/question', function(req, res) {
 		
 	});
 	
