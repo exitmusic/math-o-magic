cc58e722f0eeb8009fe5e5b1105757f02b27e645
Kai Chang <kai.h.chang@gmail.com>
2012-08-13 22:43:39 -0500

    Removed unused code from the main controller.

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -1,7 +1,5 @@
 function routes(app, io) {
   app.get('/', function(req, res){
-    //io.sockets.in(req.sessionID).send('Man, good to see you back!');
-    var numOfPlayers = io.sockets.clients('trivia-room').length;
     res.render('index', { 
       title: 'Math-o-Magic Mental Math Room'
     });
